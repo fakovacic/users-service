5270a3ff32cda9f77ca442042c4c8f7b5121df2f
fakovacic <filip.ante.kovacic@gmail.com>
2022-06-19 20:40:39 +0200

    add validation

--- internal/users/create.go
+++ internal/users/create.go
@@ -6,7 +6,20 @@ import (
 	"github.com/fakovacic/users-service/internal/users/errors"
 )
 
+func ValidateCreate(ctx context.Context, m *User) error {
+	if m.Email == "" {
+		return errors.Wrap("email is required")
+	}
+
+	return nil
+}
+
 func (s *service) Create(ctx context.Context, m *User) (*User, error) {
+	err := ValidateCreate(ctx, m)
+	if err != nil {
+		return nil, err
+	}
+
 	m.ID = s.uuidFunc().String()
 	m.CreatedAt = s.timeFunc()
 	m.UpdatedAt = s.timeFunc()
@@ -15,7 +28,7 @@ func (s *service) Create(ctx context.Context, m *User) (*User, error) {
 		m.Password = HashAndSalt(m.Password)
 	}
 
-	err := s.store.Create(ctx, m)
+	err = s.store.Create(ctx, m)
 	if err != nil {
 		return nil, errors.Wrapf(err, "create user")
 	}
